f41c4544e73dfcef59c64eb7e555f2673aa45447
Alaanaser714 <alaanaser577@gmail.com>
2025-01-29 22:28:41 +0200

    create featured books

---
+++ lib/features/home/presentation/viewmodel/cubits/featured_book_cuibt/featured_book_states.dart
@@ -0,0 +1,15 @@
+import 'package:book_app/features/home/data/models/book_model.dart';
+
+abstract class FeaturedBookStates {}
+
+class FeaturedBookInitial extends FeaturedBookStates {}
+
+class FeaturedBookSuccess extends FeaturedBookStates {
+  final List<BookModel> books;
+  FeaturedBookSuccess(this.books);
+}
+
+class FeaturedBookFailure extends FeaturedBookStates {
+  final String errMessage;
+  FeaturedBookFailure(this.errMessage);
+}
